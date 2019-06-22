72a5ec23094a944f2ee13546ed8492e6a2d495c2
Gabriel Elliot Cano <appa@appa.local>
2019-06-22 15:32:27 -0700

    Make detector a class for ipython friendly use.

--- bird_detector.py
+++ bird_detector.py
@@ -16,118 +16,102 @@ from scipy.signal import spectrogram
 import librosa
 from PIL import Image
 
+class Detector:
 
-def saveim(im, filename, size=(10, 4)):
-    pil_img = Image.fromarray((im*255).astype('uint8')).convert('RGB')
-    pil_img.save(filename)
+    def __init__(self, filename, ident=1, frame_len=1024):
+        self.clip, self.sr = librosa.load(filename)
+        self.spec = self.norm(self.get_spec(frame_len))
+        self.dets = None  # detections, set in create_segmentations
+        self.id = ident  # so files have non overlapping detection names
 
+    @staticmethod
+    def norm(spec):
+        """
+        spec - 2d numpy array - representation of clip in frequency domain
+        return - 2d numpy array - same shape, normalized between 1 and 0
+        """
+        return (spec - spec.min(0)) / spec.ptp(0)
 
-def load_clip(filepath):
-    """
-    filepath - string - full filepath to desired clip
-    return - int, 2d numpy array - sampling rate and data for the clip
-    """
-    clip, sr = librosa.load(filepath)
-    return sr, clip
+    def get_spec(self, frame_len):
+        stft_clip = librosa.stft(
+            self.clip, n_fft=frame_len, hop_length=frame_len//2+1)
+        stft_mag, stft_ph = librosa.magphase(stft_clip)
+        stft_mag_db = librosa.amplitude_to_db(stft_mag)
+        return stft_mag_db
 
+    def detect_events(self, blur, pickup):
+        """
+        Lift sound events from background noise.
+        spectrogram - 2d numpy array
+        blur - int - the window size of the gaussian blur (bigger number = more blur)
+        pickup - float [0,1] - the number above which things will be detected
+        return - 2d numpy array - a spectrogram of the same shape
+        """
+        self.spec = gaussian_filter(self.spec, blur)
+        self.spec = np.where(self.spec >= pickup, 1, 0)
+        return self.spec
 
-def get_spec(clip, frame_len):
-    stft_clip = librosa.stft(clip, n_fft=frame_len, hop_length=frame_len//2+1)
-    stft_mag, stft_ph = librosa.magphase(stft_clip)
-    stft_mag_db = librosa.amplitude_to_db(stft_mag)
-    return stft_mag_db
+    def clean_low_noise(self, threshold_row):
+        """
+        Removes all detection areas contigous to the border.
+        spectrogram - 2d numpy array
+        threshold_row - int - detections in rows below threshold row will be ignored
+        return - 2d numpy array - a spec of the same shape
+        """
+        self.spec = cpy(self.spec)
+        self.spec[:threshold_row] = 1  # create artificial detections, will be set back to 0
 
+        fill_in_inds = (self.spec==0).argmax(axis=0)  # find first nondetection in every col
 
-def norm(spec):
-    """
-    spec - 2d numpy array - representation of clip in frequency domain
-    return - 2d numpy array - same shape, normalized between 1 and 0
-    """
-    return (spec - spec.min(0)) / spec.ptp(0)
+        for c, ind in enumerate(fill_in_inds):
+            self.spec[:ind, c] = 0  # set all detections (1) previous to nondetections (0)
+        
+        return self.spec
 
+    def create_segmentations(self, blur, pickup):
+        """
+        This will split the spec up into parts.
+        spec - 2d numpy array
+        blur - int - the window for the gaussian blur (higher = less clips)
+        pickup - int - if detection strength lower will not be considered
+        return - list of 2-tuples - represents start, end of each clip
+        """
+        det_strength = np.sum(self.spec, axis=0)
+        det_strength = gaussian_filter(det_strength, blur)
+        det_strength = np.where(det_strength >= pickup, 1, 0)
 
-def detect_events(spec, blur, pickup):
-    """
-    Lift sound events from background noise.
-    spectrogram - 2d numpy array
-    blur - int - the window size of the gaussian blur (bigger number = more blur)
-    pickup - float [0,1] - the number above which things will be detected
-    return - 2d numpy array - a spectrogram of the same shape
-    """
-    s = cpy(spec)
-    s = gaussian_filter(s, blur)
-    s = np.where(s >= pickup, 1, 0)
-    return s
+        # find contigous 1s (detections)
+        self.dets = [[i for i, value in it] for key, it in itertools.groupby(
+            enumerate(det_strength), key=operator.itemgetter(1)) if key != 0]
 
+        _, w = self.spec.shape
+        for i in range(len(self.dets)):
+            start, end = (int(self.dets[i][0]*len(self.clip)/w), 
+                          int(self.dets[i][-1]*len(self.clip)/w))
+            self.dets[i] = (start, end)
 
-def clean_low_noise(spec, threshold_row):
-    """
-    Removes all detection areas contigous to the border.
-    spectrogram - 2d numpy array
-    threshold_row - int - detections in rows below threshold row will be ignored
-    return - 2d numpy array - a spec of the same shape
-    """
-    s = cpy(spec)
-    s[:threshold_row] = 1  # create artificial detections, will be set back to 0
+        return self.dets
 
-    fill_in_inds = (s==0).argmax(axis=0)  # find first nondetection in every col
-
-    for c, ind in enumerate(fill_in_inds):
-        s[:ind, c] = 0  # set all detections (1) previous to nondetections (0)
-    
-    return s
-
-
-def create_segmentations(spec, blur, pickup):
-    """
-    This will split the spec up into parts.
-    spec - 2d numpy array
-    blur - int - the window for the gaussian blur (higher = less clips)
-    pickup - int - if detection strength lower will not be considered
-    return - list of 2-tuples - represents start, end of each clip
-    """
-    detection_strength = np.sum(spec, axis=0)
-    detection_strength = gaussian_filter(detection_strength, blur)
-    detection_strength = np.where(detection_strength >= pickup, 1, 0)
-
-    # find contigous 1s (detections)
-    vocs = [[i for i, value in it] for key, it in itertools.groupby(
-        enumerate(detection_strength), key=operator.itemgetter(1)) if key != 0]
-
-    for i in range(len(vocs)):
-        start, end = vocs[i][0], vocs[i][-1]
-        vocs[i] = (start, end)
-
-    return vocs
-
-
-def write_detection_clips(clip, spec, sr, vocs):
-    """
-    Write detected clips to wav files.
-    clip - 1d numpy array - the original clip file
-    spec - 2d numpy array - needed to translate vocs to original clip
-    vocs - list of 2-tuples - represents start, end indices of clips in spectrogram
-    return - None
-    """
-    _, w = spec.shape
-
-    for i, (start, end) in enumerate(vocs):
-        new_clip = clip[int(start*len(clip)/w) : int(end*len(clip)/w)]
-        wavfile.write('./d{}.wav'.format(i), sr, new_clip)  # TODO: change so no ov
+    def write_detection_clips(self, vocs):
+        """
+        Write detected clips to wav files.
+        clip - 1d numpy array - the original clip file
+        spec - 2d numpy array - needed to translate vocs to original clip
+        vocs - list of 2-tuples - represents start, end indices of clips in spectrogram
+        return - None
+        """
+        for i, (start, end) in enumerate(vocs):
+            new_clip = self.clip[start:end]
+            wavfile.write('./dets/clip{}_d{}.wav'.format(self.id, i), self.sr, new_clip)
 
 
 def main():
     filename = '/Users/appa/birds/d_parker/5CAA70D6.WAV'
-    sr, clip = load_clip(filename)
-    s = get_spec(clip, frame_len=1024)
-    s = norm(s)
-    saveim(s, './normed.jpeg')
-    s = detect_events(s, blur=3, pickup=.65)
-    saveim(s, './detections.png')
-    s = clean_low_noise(s, threshold_row=55)
-    saveim(s, './cleaned.png')
-    vocs = create_segmentations(s, blur=19, pickup=1)
-    write_detection_clips(clip, s, sr, vocs)
+    detector = Detector(filename)
+    detector.detect_events(blur=3, pickup=.65)
+    detector.clean_low_noise(threshold_row=55)
+    vocs = detector.create_segmentations(blur=19, pickup=1)
+    detector.write_detection_clips(vocs)
 
 
 if __name__ == "__main__":
