a889e724fbf8af103204f2c28a8dceef458a2906
Gabriel Elliot Cano <appa@appa.local>
2019-07-10 15:54:34 -0700

    Add undo ability. Reorganize into one file.

--- tkinter_gui.py
+++ tkinter_gui.py
@@ -1,7 +1,7 @@
 ##################################################################################
 #    Gabriel Cano                                                                #
 ##################################################################################
-#                                                                                #
+#    A gui made for labeling sound events in a spectrogram manually.             #
 #                                                                                #
 #                                                                                #
 ##################################################################################
@@ -13,8 +13,10 @@ import os
 import csv
 import sys
 from playsound import playsound
-from det_tools import *
 import shutil
+from copy import deepcopy as cpy
+import librosa
+import numpy as np
 
 
 
@@ -30,7 +32,6 @@ class AudioAnnotator:
                 if 'wav' in filename or 'WAV' in filename:
                     self.wav_files.append(filename)
             break  # activate this line if only top level of directory wanted
-        print(self.wav_files)
 
         # saving folders
         self.save_folder = save_folder
@@ -47,7 +48,7 @@ class AudioAnnotator:
         self.spec_h = 0
 
         # variables set elsewhere that change with each new clip
-        self.f_ind = f_ind 
+        self.f_ind = f_ind
         self.curr_clip = AudioClip(os.path.join(self.data_folder, self.wav_files[self.f_ind]))
         self.canvas_img1 = None  # I have no idea why I need two, it works
         self.canvas_img2 = None
@@ -60,7 +61,7 @@ class AudioAnnotator:
         
         self.create_ui()
 
-        self.info1.set('displaying file #{}'.format(self.f_ind))
+        self.info1.set('displaying file #{} ({})'.format(self.f_ind, self.curr_filename()))
         self.info2.set('working on mini clip #{}'.format(self.d_ind))
 
         self.root.mainloop()
@@ -86,11 +87,66 @@ class AudioAnnotator:
         self.d_ind += 1  
         self.info2.set('working on mini clip #{}'.format(self.d_ind))
 
+    def undo(self):
+        curr_filename = self.curr_save_filename()
+        prev_file_ind = str(int(curr_filename.split('.')[0].split('-')[-1]) - 1)
+        prev_file = self.curr_save_filename().split('-')[0] + '-' + prev_file_ind + '.wav'
+        os.remove(os.path.join(self.save_folder, prev_file))
+
+        with open(os.path.join(self.save_folder, self.csv_filename), 'r') as csvfile:
+            r = csv.reader(csvfile)
+            rows = []
+            for row in r:
+                rows.append(row)
+
+        with open(os.path.join(self.save_folder, self.csv_filename), 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(rows[:-1])
+
+        self.info3.set('Erased {}'.format(os.path.join(self.save_folder, prev_file)))
+
+        self.d_ind -= 1  
+        self.info2.set('working on mini clip #{}'.format(self.d_ind))
+
+    def prev(self):
+        self.f_ind -= 1
+        self.info1.set('displaying file #{} ({})'.format(self.f_ind, self.curr_filename()))
+
+        # figure out what next d_ind should be
+        highest = -1
+        for filename in os.listdir(self.save_folder):
+            if self.curr_filename().split('.')[0] in filename:  # if detection pertains to current file
+                ind = int(filename.split('-')[-1].split('.')[0])
+                if ind > highest:
+                    highest = cpy(ind)
+
+        self.d_ind = highest + 1
+        self.info2.set('working on mini clip #{}'.format(self.d_ind))
+
+        self.curr_clip = AudioClip(os.path.join(self.data_folder, self.curr_filename()))  # get audioclip
+
+        # display audioclip
+        viewable_pil = Image.fromarray((self.curr_clip.spec * 255).astype('uint8'))
+        self.temp = img =  ImageTk.PhotoImage(image=viewable_pil)
+        self.canvas_img2 = self.canvas.itemconfig(self.canvas_img1, image=img)
+
+        # delete any yellow rects
+        if self.curr_rect_id:
+            self.canvas.delete(self.curr_rect_id)
+
     def next_(self):
         self.f_ind += 1
-        self.info1.set('displaying file #{}'.format(self.f_ind))
+        self.info1.set('displaying file #{} ({})'.format(self.f_ind, self.curr_filename()))
 
-        self.d_ind = 0  
+        # figure out what next d_ind should be
+        highest = -1
+        for filename in os.listdir(self.save_folder):
+            if self.curr_filename().split('.')[0] in filename:  # if detection pertains to current file
+                ind = int(filename.split('-')[-1].split('.')[0])
+                if ind > highest:
+                    highest = cpy(ind)
+
+        self.d_ind = highest + 1
         self.info2.set('working on mini clip #{}'.format(self.d_ind))
 
         self.curr_clip = AudioClip(os.path.join(self.data_folder, self.curr_filename()))  # get audioclip
@@ -149,6 +205,8 @@ class AudioAnnotator:
         self.canvas.bind("<ButtonRelease-1>", self.mouse_up)
         self.canvas_img1 = self.canvas.create_image(0, 0, image=img, anchor='nw')
 
+        # self.canvas.bind_all('<MouseWheel>', lambda e: self.canvas.xview_scroll(-1 * e.delta, 'units'))
+
         frame.pack(side=tk.TOP)
 
         frame = ttk.Frame(self.root)
@@ -166,18 +224,21 @@ class AudioAnnotator:
 
         # create & place far right side buttons
         ttk.Button(frame, text='save', command=self.save).grid(row=1, column=3)
-        ttk.Button(frame, text='next', command=self.next_).grid(row=1, column=4)
-        ttk.Button(frame, text='exit', command=self.exit).grid(row=1, column=5)
+        ttk.Button(frame, text='back', command=self.prev).grid(row=1, column=4)
+        ttk.Button(frame, text='next', command=self.next_).grid(row=1, column=5)
+        ttk.Button(frame, text='exit', command=self.exit).grid(row=1, column=6)
 
         # informational labels
-        ttk.Label(frame, textvariable=self.info1).grid(row=1, column=6)
-        ttk.Label(frame, textvariable=self.info2).grid(row=1, column=7)
+        ttk.Label(frame, textvariable=self.info1).grid(row=1, column=7)
+        ttk.Label(frame, textvariable=self.info2).grid(row=1, column=8)
         ttk.Label(frame, textvariable=self.info3).grid(row=2, column=0, columnspan=8, sticky='w')
 
         # create key bindings for play, save, next, & exit
         self.root.bind_all('<Command-KeyPress-p>', lambda _: self.play())
+        self.root.bind_all('<Command-KeyPress-u>', lambda _: self.undo())
         self.root.bind_all('<Command-KeyPress-s>', lambda _: self.save())
         self.root.bind_all('<Command-KeyPress-n>', lambda _: self.next_())
+        self.root.bind_all('<Command-KeyPress-b>', lambda _: self.prev())
         self.root.bind_all('<Escape>', lambda _: self.exit())
         
         frame.pack(side=tk.BOTTOM, fill=tk.BOTH)
@@ -210,6 +271,43 @@ class SaveSessionPopup():
 
     def exit(self):
         self.root.quit()
+
+
+class AudioClip:
+    def __init__(self, filename, frame_len=1024):
+        self.clip, self.sr = librosa.load(filename)
+
+        # creates spectrogram
+        stft_clip = librosa.stft(self.clip, n_fft=frame_len, hop_length=frame_len//2+1)
+        stft_mag, stft_ph = librosa.magphase(stft_clip)
+        self.spec = librosa.amplitude_to_db(stft_mag)
+        self.spec = self.norm(self.spec)  # norm between 0 and 1
+        self.spec = self.spec[np.where(np.sum(self.spec, axis=1) > 1)]
+        self.spec = np.flipud(self.spec)  # flip so low sounds are on bottom
+
+    @staticmethod
+    def norm(spec):
+        """
+        spec - 2d numpy array - representation of clip in frequency domain
+        return - 2d numpy array - same shape, normalized between 0 and 1
+        """
+        min_num = np.amin(spec)
+        max_num = np.amax(spec)
+        return np.divide(np.add(spec, -min_num), max_num-min_num)
+
+    def write_mini_clip(self, filename, start_spec, end_spec):
+        """
+        filename - string - the full filepath to save the clip
+        start_ind - int - the desired starting place in spec
+        end_ind - int - the desired stopping place in spec
+        return - None
+        """
+        _, spec_w = self.spec.shape
+        start = int(start_spec * len(self.clip) / spec_w)
+        end   = int(end_spec   * len(self.clip) / spec_w)
+
+        mini = self.clip[start:end]
+        librosa.output.write_wav(filename, mini, self.sr)
 
 
 if __name__ == '__main__':
